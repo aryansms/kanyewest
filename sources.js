2344432b2f198f6beaf0d70384af8487de7fd3aa
AryanSMS <160195191+aryansms@users.noreply.github.com>
2024-04-07 14:03:45 -0400

    initial release (should finish?)

---
+++ sources.js
@@ -0,0 +1,13 @@
+const textElement = document.getElementById('text');
+const text = 'SOURCES            ...            LOADING             //            KANYE WEST IMAGE: PHOTO BY JAMEL TOPPIN ON FORBES // CAR CRASH IMAGE: LAWBYYOURSIDE // KANYE MAD AT INTERVIEWER: GMA NEWS // ALL KANYE ALBUM COVERS: SPOTIFY // ALL FAIR USE EXCEPTIONS. NO MONEY IS MADE FROM THIS PAGE. // MADE WITH 💚 BY ARYAN J :)';
+let index = 0;
+
+function type() {
+    if (index < text.length) {
+        textElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, 50); // Adjust speed as needed (milliseconds)
+    }
+} 
+
+type();
